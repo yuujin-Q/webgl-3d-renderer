f7d2eb76ab154be0286d9899d0f450ddcb3eacc0
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-05-03 08:27:45 +0700

    feat: init canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { useRef } from "react"
 import Canvas from "./components/Canvas"
 import Header from "./components/Header"
 import Leftbar from "./components/Leftbar"
 import Rightbar from "./components/Rightbar"
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   return (
     <>
       <Header/>
       <div className="flex">
         <Leftbar/>
-        <Canvas/>
+        <Canvas canvasRef={canvasRef}/>
         <Rightbar/>
       </div>
     </>
