adf4bc12e8b0b88db75528ed94e2f9e666db046a
Nigel Sahl <93074692+NerbFox@users.noreply.github.com>
2024-05-13 20:42:11 +0700

    feat: BufferGeometry

---
+++ src/types/objects/bufferattribute.ts
@@ -0,0 +1,140 @@
+type TypedArray = Float32Array | Uint8Array | Uint16Array | Uint32Array | Int8Array | Int16Array | Int32Array;
+
+export class BufferAttribute {
+    private _data: TypedArray;
+    private _size: number;
+    private _dtype: number;
+    private _normalize = false;
+    private _stride = 0;
+    private _offset = 0;
+
+    private _isDirty = true; // kita copy atribut minimal sekali di awal terlebih dahulu
+
+    /**
+     * Creates an instance of BufferAttribute.
+     * @param {TypedArray} data Typed array data.
+     * @param {number} size Size of each element in the buffer.
+     * @param {object} options Options for attribute.
+     * @memberof BufferAttribute
+     */
+
+    constructor(
+        data: TypedArray,
+        size: number,
+        options: {
+            dtype?: number,
+            normalize?: boolean,
+            stride?: number,
+            offset?: number,
+        } = {},
+    ) {
+        this._data      = data;
+        this._size      = size;
+        this._dtype     = options.dtype     || WebGLRenderingContext.FLOAT;
+        this._normalize = options.normalize || false;
+        this._stride    = options.stride    || 0;
+        this._offset    = options.offset    || 0;
+    }
+
+
+    // Public get accessor to private properties.
+    get data() { return this._data; }
+    get size() { return this._size; }
+    get dtype() { return this._dtype; }
+    get normalize() { return this._normalize; }
+    get stride() { return this._stride; }
+    get offset() { return this._offset; }
+    get isDirty() { return this._isDirty; }
+    // Public set accessor to private properties.
+    // Should toggle isDirty flag to true.
+    set data(data: TypedArray) {
+        this._data = data;
+        this._isDirty = true;
+    }
+    set size(size: number) {
+        this._size = size;
+        this._isDirty = true;
+    }
+    set dtype(dtype: number) {
+        this._dtype = dtype;
+        this._isDirty = true;
+    }
+    set normalize(normalize: boolean) {
+        this._normalize = normalize;
+        this._isDirty = true;
+    }
+    set stride(stride: number) {
+        this._stride = stride;
+        this._isDirty = true;
+    }
+    set offset(offset: number) {
+        this._offset = offset;
+        this._isDirty = true;
+    }
+
+
+    /**
+     * Tandai buffer sebagai bersih
+     * (tidak perlu di-copy kembali ke GPU)
+     *
+     * Hanya dipanggil pada attribute setter.
+     */
+    consume() {
+        this._isDirty = false;
+    }
+
+
+    /**
+     * Jumlah elemen dalam buffer (elemen = data.length / size).
+     */
+    get count() {
+        return this._data.length / this._size;
+    }
+
+
+    /**
+     * Panjang dari buffer (data.length = elemen * size).
+     */
+    get length() {
+        return this._data.length;
+    }
+
+
+    set(index: number, data: number[]) {
+        this._isDirty = true;
+        // Set elemen[index] dengan data (data.length == this._size)
+        // Jangan lupa untuk menyesuaikan dengan offset dan stride.
+        const start = index * this._size + this._offset;
+        for (let i = 0; i < this._size; i++) {
+            this._data[start + i] = data[i];
+        }
+        this.offset += this.stride;
+    }
+
+
+    get(index: number, size?: number) {
+        index *= this._size;
+        if (!size) size = this._size;
+        const data: number[] = [];
+        // Ambil elemen[index] ke data (data.length == size)
+        // Jangan lupa untuk menyesuaikan dengan offset dan stride.
+        const start = index + this._offset;
+        for (let i = 0; i < size; i++) {
+            data.push(this._data[start + i]);
+        }
+        return data;
+    }
+}
+
+// const attr = new BufferAttribute(new Float32Array([
+//     -2,  2, // A
+//     -2, -2, // B
+//      2, -2, // C
+//      2, -2, // C
+//      2,  2, // D
+//     -2,  2, // A
+// ]), 2);
+// attr.count; // 6: A, B, C, C, D, A
+// attr.length; // 12
+// attr.set(0, [-2, 0]); // set vertex 0 jadi (-2, 0)
+// attr.get(0); // [ -2, 0 ]
---
+++ src/types/objects/buffergeometry.ts
@@ -0,0 +1,118 @@
+import {BufferAttribute} from './bufferattribute';
+
+export class BufferGeometry {
+    private _attributes: {[name: string]: BufferAttribute};
+    private _indices?: BufferAttribute;
+
+
+    constructor() {
+        this._attributes = {};
+    }
+
+
+    get attributes() {
+        return this._attributes;
+    }
+
+
+    get indices() {
+        return this._indices;
+    }
+
+
+    setIndices(indices: BufferAttribute) {
+        this._indices = indices;
+        return this;
+    }
+
+
+    removeIndices() {
+        this._indices = undefined;
+        return this;
+    }
+
+
+    setAttribute(name: string, attribute: BufferAttribute) {
+        this._attributes[name] = attribute;
+        return this;
+    }
+
+
+    getAttribute(name: string) {
+        return this._attributes[name];
+    }
+
+
+    deleteAttribute(name: string) {
+        delete this._attributes[name];
+        return this;
+    }
+
+
+    calculateNormals(forceNewAttribute=false) {
+        const position = this.getAttribute('position');
+        if (!position) return;
+        let normal = this.getAttribute('normal');
+        if (forceNewAttribute || !normal)
+            normal = new BufferAttribute(new Float32Array(position.length), position.size);
+        else
+            normal.data.fill(0); // Reset normal data to 0.
+        // Lakukan kalkulasi normal disini.
+
+        const indices = this.indices?.data;
+        const indexCount = indices ? indices.length : position.count;
+        const posData = position.data;
+        const normData = normal.data;
+
+        for (let i = 0; i < indexCount; i += 3) {
+            // Ambil index dari vertex A, B, dan C.
+            const a = indices ? indices[i] : i;
+            const b = indices ? indices[i + 1] : i + 1;
+            const c = indices ? indices[i + 2] : i + 2;
+
+            // Ambil posisi dari vertex A, B, dan C.
+            const p0 = posData.slice(a * 3, a * 3 + 3);
+            const p1 = posData.slice(b * 3, b * 3 + 3);
+            const p2 = posData.slice(c * 3, c * 3 + 3);
+            
+            // Hitung vektor normal dari segitiga ABC.
+            const u = [p1[0] - p0[0], p1[1] - p0[1], p1[2] - p0[2]];
+            const v = [p2[0] - p0[0], p2[1] - p0[1], p2[2] - p0[2]];
+
+            const n = [
+                u[1] * v[2] - u[2] * v[1],
+                u[2] * v[0] - u[0] * v[2],
+                u[0] * v[1] - u[1] * v[0]
+            ];
+
+            normData[a * 3] += n[0];
+            normData[a * 3 + 1] += n[1];
+            normData[a * 3 + 2] += n[2];
+
+            normData[b * 3] += n[0];
+            normData[b * 3 + 1] += n[1];
+            normData[b * 3 + 2] += n[2];
+
+            normData[c * 3] += n[0];
+            normData[c * 3 + 1] += n[1];
+            normData[c * 3 + 2] += n[2];
+        }
+
+        // Normalisasi vektor normal.
+        for (let i = 0; i < normData.length; i += 3) {
+            const x = normData[i];
+            const y = normData[i + 1];
+            const z = normData[i + 2];
+            
+            const length = Math.sqrt(x * x + y * y + z * z);
+            if (length === 0) continue;
+
+            normData[i] /= length;
+            normData[i + 1] /= length;
+            normData[i + 2] /= length;
+        }
+
+        this.setAttribute('normal', normal);
+        return this;
+    }
+}
