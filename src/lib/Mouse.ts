d1c0c97f759f60f4e5b3f30fd44c2f3059184f6b
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-05-18 21:15:59 +0700

    refactor: mouse

--- src/lib/Mouse.ts
+++ src/lib/Mouse.ts
@@ -2,48 +2,48 @@ import { ObjectNode } from "../types/objects/ObjectNode";
 import { Renderer } from "./Renderer";
 
 export class MouseInput {
-  static initPhi = 0;
-  static initTheta = 0;
-  static deltaPhi = 0;
-  static deltaTheta = 0;
-  static befPhi = 0;
-  static befTheta = 0;
+  private static initPhi = 0;
+  private static initTheta = 0;
+  private static deltaPhi = 0;
+  private static deltaTheta = 0;
+  private static prevDeltaPhi = 0;
+  private static prevDeltaTheta = 0;
+  static sensitivity = 0.0001;
   static camera: ObjectNode;
 
   static initAngles(e: React.MouseEvent<HTMLCanvasElement>) {
     this.initPhi = e.clientX;
     this.initTheta = e.clientY;
   }
-  
+
   static onMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    this.befPhi = this.deltaPhi;
-    this.befTheta = this.deltaTheta;
+    this.prevDeltaPhi = this.deltaPhi;
+    this.prevDeltaTheta = this.deltaTheta;
     this.deltaPhi = e.clientX - this.initPhi;
     this.deltaTheta = e.clientY - this.initTheta;
 
-    const isIncreasePhi = this.deltaPhi > this.befPhi;
-    const isIncreaseTheta = this.deltaTheta > this.befTheta;
-    
-    const x = this.camera.position.x * Math.sin(this.deltaPhi) * Math.cos(this.deltaTheta);
+    const isIncreasePhi = this.deltaPhi > this.prevDeltaPhi;
+    const isIncreaseTheta = this.deltaTheta > this.prevDeltaTheta;
+
+    const x =
+      this.camera.position.x *
+      Math.sin(this.deltaPhi) *
+      Math.cos(this.deltaTheta);
     const y = this.camera.position.y * Math.cos(this.deltaPhi);
-    const z = this.camera.position.z * Math.sin(this.deltaPhi) * Math.sin(this.deltaTheta);
+    const z =
+      this.camera.position.z *
+      Math.sin(this.deltaPhi) *
+      Math.sin(this.deltaTheta);
     this.camera.position = { x, y, z };
 
-    // const xRot = Math.atan2(z, y) / 100;
-    // const yRot = Math.atan2(x, z) / 100;
-    // const zRot = Math.atan2(y, x) / 100;
-    // this.camera.rotation = { x: xRot, y: yRot, z: zRot };
-
     // increase rotation camera with deltaPhi and deltaTheta
-    const sensitivity = 0.0001;
-    this.camera.rotation.x -= Math.abs(this.deltaTheta * sensitivity) * (isIncreaseTheta ? 1 : -1)
-    this.camera.rotation.y -= Math.abs(this.deltaPhi * sensitivity) * (isIncreasePhi ? 1 : -1)
-    // this.camera.rotation.z
-    
-    this.camera.computeWorldMatrix();
+    this.camera.rotation.x -=
+      Math.abs(this.deltaTheta * this.sensitivity) * (isIncreaseTheta ? 1 : -1);
+    this.camera.rotation.y -=
+      Math.abs(this.deltaPhi * this.sensitivity) * (isIncreasePhi ? 1 : -1);
+
     // Render the scene
+    this.camera.computeWorldMatrix();
     Renderer.renderScene();
-    // console.log("camera.rotation: ", this.camera.rotation);
-    // console.log(this.deltaPhi, this.deltaTheta);\
-  }
+  };
 }
