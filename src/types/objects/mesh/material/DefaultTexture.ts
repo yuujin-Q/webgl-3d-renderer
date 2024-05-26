40e2f23e39543e626aa1732565aa750185a6aa15
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-05-26 20:00:32 +0700

    default brick texture

---
+++ src/types/objects/mesh/material/DefaultTexture.ts
@@ -0,0 +1,27 @@
+import { Texture } from "./Texture";
+
+type TexturePreset = {
+  u_diffuseMap: Texture;
+  u_specularMap?: Texture;
+  u_normalMap?: Texture;
+  u_displacementMap?: Texture;
+};
+
+// Brick
+const brickDiffuse = new Texture();
+brickDiffuse.setData("/brick-wall/brick-wall_albedo.png");
+const brickSpecular = new Texture();
+brickSpecular.setData("/brick-wall/brick-wall_ao.png");
+const brickNormalMap = new Texture();
+brickNormalMap.setData("/brick-wall/brick-wall_normal.png");
+const brickDisplacementMap = new Texture();
+brickDisplacementMap.setData("/brick-wall/brick-wall_height.png");
+
+const brickPreset: TexturePreset = {
+  u_diffuseMap: brickDiffuse,
+  u_specularMap: brickSpecular,
+  u_normalMap: brickNormalMap,
+  u_displacementMap: brickDisplacementMap,
+};
+
+export { brickPreset };
